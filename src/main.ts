5006587aa3e45239ca1c04bcc3f38b541d02fa63
Yuta Ohira <50891407+Alesion30@users.noreply.github.com>
2025-01-25 16:07:48 +0900

    feat: streaming

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ const ollama = new Ollama({ host: "http://127.0.0.1:11434" });
 const response = await ollama.chat({
   model: "llama3.2",
   messages: [{ role: "user", content: "なぜ空は青いのですか？" }],
+  stream: true,
 });
+
 console.log("ollama response: \n");
-console.log(response.message.content);
+for await (const part of response) {
+  process.stdout.write(part.message.content);
+}
