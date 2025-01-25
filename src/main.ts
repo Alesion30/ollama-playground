69eda4b85efc7aeb1cb6d5afa38e130c27d6fa83
Yuta Ohira <50891407+Alesion30@users.noreply.github.com>
2025-01-25 16:25:10 +0900

    feat: get models

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import { Ollama } from "ollama";
 
 const ollama = new Ollama({ host: "http://127.0.0.1:11434" });
 
+const { models } = await ollama.list();
+console.log("========================================");
+console.log("available models:");
+console.log("========================================");
+console.log(models);
+
 const response = await ollama.chat({
   model: "llama3.2",
   messages: [{ role: "user", content: "なぜ空は青いのですか？" }],
   stream: true,
 });
 
-console.log("ollama response: \n");
+console.log("========================================");
+console.log("chat response:");
+console.log("========================================");
 for await (const part of response) {
   process.stdout.write(part.message.content);
 }
