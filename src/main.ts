d9c7cdfebbd9c3041434192164e2001545665837
Yuta Ohira <50891407+Alesion30@users.noreply.github.com>
2025-01-25 16:38:12 +0900

    refactor: comment out model listing and logging

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { Ollama } from "ollama";
 
 const ollama = new Ollama({ host: "http://127.0.0.1:11434" });
 
-const { models } = await ollama.list();
-console.log("========================================");
-console.log("available models:");
-console.log("========================================");
-console.log(models);
+// const { models } = await ollama.list();
+// console.log("========================================");
+// console.log("available models:");
+// console.log("========================================");
+// console.log(models);
 
 const response = await ollama.chat({
   model: "llama3.2",
